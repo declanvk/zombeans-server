a7e02f5772cabc7e6f3905bf67be0fc03595cab2
Declan Kelly <dkelly.home@gmail.com>
2019-02-01 21:55:59 -0800

    Implement safe player dis/connect

--- server.py
+++ server.py
@@ -19,18 +19,22 @@ STATIC_FOLDER = getenv("ZOMBEANS_STATIC_FOLDER", default='static')
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 app.config['STATIC_FOLDER'] = Path(STATIC_FOLDER)
-app.config['SECRET_KEY'] = "hey you! don't you dare say anything. Snitches get stitches"
+app.config[
+    'SECRET_KEY'] = "hey you! don't you dare say anything. Snitches get stitches"
 socketio = SocketIO(app, logger=logger)
+
 
 # Handle first page
 @app.route('/')
 def main_page():
     return send_from_directory(app.config['STATIC_FOLDER'], 'index.html')
 
+
 # Handle all static resources
 @app.route('/<path:subpath>')
 def static_content(subpath):
     return send_from_directory(app.config['STATIC_FOLDER'], subpath)
+
 
 # Game States
 GAME_STATE_LOBBY_WAITING = 1
@@ -48,8 +52,10 @@ PLAYER_STATE_IN_GAME = 3
 #   - Game (host): is served game rendering events
 #   - Viewer (optional): is served game rendering events
 
+
 class Server:
-    def __init__(self, host_namespace_class, viewer_namespace_class, player_namespace_class):
+    def __init__(self, host_namespace_class, viewer_namespace_class,
+                 player_namespace_class):
         self.host_namespace_class = host_namespace_class
         self.viewer_namespace_class = viewer_namespace_class
         self.player_namespace_class = player_namespace_class
@@ -66,12 +72,15 @@ class Server:
     def generate_room_code(self):
         return ''.join(choices(ascii_lowercase, k=IDENTIFIER_LEN)).upper()
 
-    def register(self, socket_io)
+    def register(self, socket_io):
         self.socket_io = socket_io
 
-        self.host_namespace = self.host_namespace_class(HOST_ENDPOINT, parent=self)
-        self.viewer_namespace = self.viewer_namespace_class(VIEWER_ENDPOINT, parent=self)
-        self.player_namespace = self.player_namespace_class(PLAYER_ENDPOINT, parent=self)
+        self.host_namespace = self.host_namespace_class(
+            HOST_ENDPOINT, parent=self)
+        self.viewer_namespace = self.viewer_namespace_class(
+            VIEWER_ENDPOINT, parent=self)
+        self.player_namespace = self.player_namespace_class(
+            PLAYER_ENDPOINT, parent=self)
 
         self.socket_io.on_namespace(self.host_namespace)
         self.socket_io.on_namespace(self.viewer_namespace)
@@ -89,24 +98,31 @@ class Server:
         self.host_namespace.send_room_code(host_id, room_code)
 
     def register_host_disconnect(self, host_id):
-        game_state = self.hosts[host_id]['game_state']
-        if game_state == GAME_STATE_LOBBY_WAITING:
-            pass
-        elif game_state == GAME_STATE_LOBBY_WAITING:
-            pass
-        elif game_state == GAME_STATE_LOBBY_WAITING:
-            pass
-        elif game_state == GAME_STATE_LOBBY_WAITING:
-            pass
+        host = self.hosts[host_id]
+        game_state = host['game_state']
+        players = host['players']
+        room_code = host['room_code']
+
+        if (game_state == GAME_STATE_LOBBY_WAITING) or (
+                game_state == GAME_STATE_STARTING) or (
+                    game_state == GAME_STATE_RUNNING):
+            for player_id in players:
+                self.players[player_id]['game_host'] = None
+                self.players[player_id]['state'] = PLAYER_STATE_NOTHING
+
+            self.player_namespace.broadcast_game_over(room_code)
+            logger.warn(
+                "Host disconnected while attending to game (id: {}, room: {})".
+                format(host_id, room_code))
         else:
-            pass
+            logger.warn(
+                "Host disconnected with invalid state (id: {}, state: {})".
+                format(host_id, game_state))
 
         del self.hosts[host_id]
 
     def register_viewer_connect(self, viewer_id):
-        self.viewers[viewer_id] = {
-            'room_code': None
-        }
+        self.viewers[viewer_id] = {'room_code': None}
 
     def register_viewer_disconnect(self, viewer_id):
         del self.viewers[viewer_id]
@@ -115,22 +131,28 @@ class Server:
         self.players[player_id] = {
             'user_name': None,
             'character': None,
-            'state': PLAYER_STATE_NOTHING
+            'state': PLAYER_STATE_NOTHING,
+            'game_host': None,
         }
 
     def register_player_disconnect(self, player_id):
-        player_state = self.players[player_id]['state']
+        player = self.players[player_id]
+        player_state = player['state']
+        game_host = player['game_host']
 
-        if player_state == PLAYER_STATE_NOTHING:
-            pass
-        elif player_state == PLAYER_STATE_WAITING_GAME_START:
-            pass
-        elif player_state == PLAYER_STATE_IN_GAME:
-            pass
+        if (player_state == PLAYER_STATE_WAITING_GAME_START) or (
+                player_state == PLAYER_STATE_IN_GAME):
+            self.hosts[game_host]['players'].remove(player_id)
+            logger.warn(
+                "Player disconnected while game was running (id: {}, game: {})"
+                .format(player_id, game_host))
         else:
-            pass
+            logger.warn(
+                "Player disconnected with invalid state (id: {}, state: {})".
+                format(player_id, player_state))
 
         del self.players[player_id]
+
 
 server = Server(HostNamespace, ViewerNamespace, PlayerNamespace)
 server.register(socketio)
