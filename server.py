ae4a51e92000bb0b5b4b5c961fd2029e762b7c7b
Declan Kelly <dkelly.home@gmail.com>
2019-02-01 21:27:31 -0800

    Add initialization and cleanup for all types of clients

--- server.py
+++ server.py
@@ -32,6 +32,17 @@ def main_page():
 def static_content(subpath):
     return send_from_directory(app.config['STATIC_FOLDER'], subpath)
 
+# Game States
+GAME_STATE_LOBBY_WAITING = 1
+GAME_STATE_STARTING = 2
+GAME_STATE_RUNNING = 3
+GAME_STATE_FINISHED = 4
+
+# Player States
+PLAYER_STATE_NOTHING = 1
+PLAYER_STATE_WAITING_GAME_START = 2
+PLAYER_STATE_IN_GAME = 3
+
 # Types of clients:
 #   - Player: is served controls and joins a room and plays a game
 #   - Game (host): is served game rendering events
@@ -52,7 +63,7 @@ class Server:
         # Map web connection id to all web data
         self.web_connections = {}
 
-    def generate_lounge_id(self):
+    def generate_room_code(self):
         return ''.join(choices(ascii_lowercase, k=IDENTIFIER_LEN)).upper()
 
     def register(self, socket_io)
@@ -66,24 +77,60 @@ class Server:
         self.socket_io.on_namespace(self.viewer_namespace)
         self.socket_io.on_namespace(self.player_namespace)
 
-    def register_host_connect(self, player_id):
-        pass
+    def register_host_connect(self, host_id):
+        room_code = self.generate_room_code()
 
-    def register_host_disconnect(self, player_id):
-        pass
+        self.hosts[host_id] = {
+            'players': [],
+            'room_code': room_code,
+            'game_state': GAME_STATE_LOBBY_WAITING
+        }
 
-    def register_viewer_connect(self, player_id):
-        pass
+        self.host_namespace.send_room_code(host_id, room_code)
 
-    def register_viewer_disconnect(self, player_id):
-        pass
+    def register_host_disconnect(self, host_id):
+        game_state = self.hosts[host_id]['game_state']
+        if game_state == GAME_STATE_LOBBY_WAITING:
+            pass
+        elif game_state == GAME_STATE_LOBBY_WAITING:
+            pass
+        elif game_state == GAME_STATE_LOBBY_WAITING:
+            pass
+        elif game_state == GAME_STATE_LOBBY_WAITING:
+            pass
+        else:
+            pass
+
+        del self.hosts[host_id]
+
+    def register_viewer_connect(self, viewer_id):
+        self.viewers[viewer_id] = {
+            'room_code': None
+        }
+
+    def register_viewer_disconnect(self, viewer_id):
+        del self.viewers[viewer_id]
 
     def register_player_connect(self, player_id):
-        pass
+        self.players[player_id] = {
+            'user_name': None,
+            'character': None,
+            'state': PLAYER_STATE_NOTHING
+        }
 
     def register_player_disconnect(self, player_id):
-        pass
+        player_state = self.players[player_id]['state']
 
+        if player_state == PLAYER_STATE_NOTHING:
+            pass
+        elif player_state == PLAYER_STATE_WAITING_GAME_START:
+            pass
+        elif player_state == PLAYER_STATE_IN_GAME:
+            pass
+        else:
+            pass
+
+        del self.players[player_id]
 
 server = Server(HostNamespace, ViewerNamespace, PlayerNamespace)
 server.register(socketio)
