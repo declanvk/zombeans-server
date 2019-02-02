7c6757a37c696d128f64f3d0c05970ab040a1770
Declan Kelly <dkelly.home@gmail.com>
2019-02-01 17:59:37 -0800

    Add initial process file and release script

---
+++ scripts/make_release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ORIGINAL_DIR=${PWD}
+
+# Do not move this script from the scripts directory, it requires this and the relative path to website/
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+git checkout release
+git merge master
+git diff-index --quiet HEAD
+git commit || echo
+
+${SCRIPTS_DIR}/build_static.sh
+
+WEBSITE_DIR="${SCRIPTS_DIR}/../website"
+
+rm -r $SCRIPTS_DIR/../static/
+cp -R $WEBSITE_DIR/dist static/
+
+git push heroku-dev release:master
+git push heroku release:master
+
+git checkout master
