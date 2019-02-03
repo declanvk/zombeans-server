aaca70c78dcbbbe5d0054ce92a90107d8560b00c
Declan Kelly <dkelly.home@gmail.com>
2019-02-02 16:12:09 -0800

    Separate release script into update and release

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -7,18 +7,29 @@ ORIGINAL_DIR=${PWD}
 # Do not move this script from the scripts directory, it requires this and the relative path to website/
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-git checkout release
-git merge master
-git diff-index --quiet HEAD
-git commit || echo
-
-${SCRIPTS_DIR}/build_static.sh
-
 WEBSITE_DIR="${SCRIPTS_DIR}/../website"
 
+# Move into release brnach to perofm all work that will never leave
+git checkout release
+
+# Checkout latest master to see if anything has changed recently
+git merge master
+
+# Check if anything is out of place in repo
+git diff-index --quiet HEAD
+git status --porcelain
+
+# Build static to check if they have changed since last release
+${SCRIPTS_DIR}/build_static.sh
 rm -r $SCRIPTS_DIR/../static/
 cp -R $WEBSITE_DIR/dist static/
 
+# Check if anything is out of place in repo
+git diff-index --quiet HEAD
+git status --porcelain
+
+# If we get to this point, release is on the point of both server and web master
 git push heroku release:master
 
+# Go back
 git checkout master
---
+++ scripts/update_release.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ORIGINAL_DIR=${PWD}
+
+# Do not move this script from the scripts directory, it requires this and the relative path to website/
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+WEBSITE_DIR="${SCRIPTS_DIR}/../website"
+
+# Move into release for all dirty work
+git checkout release
+
+# Get the latest changes in master
+git merge master
+
+# And check that they merged cleanly
+git diff-index --quiet HEAD
+
+# If it needs a commit, add and echo to catch if no release is needed
+git commit -m "Update server" || echo
+
+# Build static resources and move into
+${SCRIPTS_DIR}/build_static.sh
+rm -r $SCRIPTS_DIR/../static/
+cp -R $WEBSITE_DIR/dist static/
+
+# Add all changes to static
+git add static
+
+# If it needs a commit, add and echo to catch if no release is needed
+git commit -m "Update static resources" || echo
+
+git checkout master
+
